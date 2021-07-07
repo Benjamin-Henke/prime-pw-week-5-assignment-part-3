ff0be7768d7f238d22ae4542ad447371efc4b0da
Benjamin Henke <benjaminmhenke@gmail.com>
2021-07-07 15:18:52 -0500

    Added 6 albums to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,22 +10,11 @@ function addToCollection(title, artist, yearPublished){
   return aboutRecord;
 } // end addToCollection
 
-/*
-const albumOne = {
-  title: 'Led Zeppelin',
-  artist: 'Led Zeppelin',
-  yearPublished: 1969
-}
-const albumTwo = {
-  title: 'Man on the Moon: End of the Day',
-  artist: 'Kid Cudi',
-  yearPublished: 1969
-}
-const albumThree = {
-  title: 'The Search',
-  artist: 'NF',
-  yearPublished: 2019
-}
-*/
-
+// adding albums to collection with addToCollection
 console.log('Add Album:', addToCollection('Led Zeppelin', 'Led Zeppelin', 1969));
+console.log('Add Album:', addToCollection('Man on the Moon: End of the Day', 'Kid Cudi', 2009));
+console.log('Add Album:', addToCollection('The Search', 'NF', 2019));
+console.log('Add Album:', addToCollection('Language & Perspective', 'Bad Suns', 2014));
+console.log('Add Album:', addToCollection('Too Low For Zero', 'Elton John', 1983));
+console.log('Add Album:', addToCollection('40oz. To Freedom', 'Sublime', 1992));
+console.log(collection);
