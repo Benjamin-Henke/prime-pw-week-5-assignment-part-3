8bc54ef59edab80a12ed2d51630c25b15bb3227c
Benjamin Henke <benjaminmhenke@gmail.com>
2021-07-07 15:11:19 -0500

    Created addToCollection function and tested successfully

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,31 @@
 console.log('***** Music Collection *****')
+
+// declaring collection
+let collection = [];
+
+// delcaring addToCollection
+function addToCollection(title, artist, yearPublished){
+  var aboutRecord = {title: title, artist: artist, yearPublished: yearPublished};
+  collection.push(aboutRecord);
+  return aboutRecord;
+} // end addToCollection
+
+/*
+const albumOne = {
+  title: 'Led Zeppelin',
+  artist: 'Led Zeppelin',
+  yearPublished: 1969
+}
+const albumTwo = {
+  title: 'Man on the Moon: End of the Day',
+  artist: 'Kid Cudi',
+  yearPublished: 1969
+}
+const albumThree = {
+  title: 'The Search',
+  artist: 'NF',
+  yearPublished: 2019
+}
+*/
+
+console.log('Add Album:', addToCollection('Led Zeppelin', 'Led Zeppelin', 1969));
