bdb09df8ed9ac910d581781ccd29bd8db5b1ffe0
Benjamin Henke <benjaminmhenke@gmail.com>
2021-07-08 21:35:55 -0500

    Created showCollection function. Tested successfully.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,11 +10,24 @@ function addToCollection(title, artist, yearPublished){
   return aboutRecord;
 } // end addToCollection
 
-// adding albums to collection with addToCollection
+// declaring showCollection
+function showCollection(array) {
+  console.log(`Number of records in our collection: ${collection.length}`);
+  for (let item of collection) {
+    console.log(`${item.title} by ${item.artist}, published in ${item.yearPublished}.`);
+  } // end for loop
+} // end showCollection
+
+
+// calling addToCollectionto test. adding albums to collection.
 console.log('Add Album:', addToCollection('Led Zeppelin', 'Led Zeppelin', 1969));
 console.log('Add Album:', addToCollection('Man on the Moon: End of the Day', 'Kid Cudi', 2009));
 console.log('Add Album:', addToCollection('The Search', 'NF', 2019));
 console.log('Add Album:', addToCollection('Language & Perspective', 'Bad Suns', 2014));
 console.log('Add Album:', addToCollection('Too Low For Zero', 'Elton John', 1983));
 console.log('Add Album:', addToCollection('40oz. To Freedom', 'Sublime', 1992));
+// viewing collection
 console.log(collection);
+
+// call showCollection to test
+showCollection(collection);
